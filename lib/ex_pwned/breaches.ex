4ae3d3054e94ed04a81cdd2bd21a775dd39a2fb1
Samar Dhwoj Acharya <coolsamar207@gmail.com>
2016-12-10 15:22:39 -0600

    update docs and add breaches?/1 shortcut (#1)

--- lib/ex_pwned/breaches.ex
+++ lib/ex_pwned/breaches.ex
@@ -20,17 +20,55 @@ defmodule ExPwned.Breaches do
 
   @doc """
   Get all breaches in hibp.
+
+  ## Examples
+
+      iex> ExPwned.Breaches.breaches
+      {:ok,
+      [%{"AddedDate" => "2015-10-26T23:35:45Z", "BreachDate" => "2015-03-01",
+          "DataClasses" => ["Email addresses", "IP addresses", "Names", "Passwords"],
+          "Description" => "In approximately March 2015, the free web hosting provider <a href=\"http://www.troyhunt.com/2015/10/breaches-traders-plain-text-passwords.html\" target=\"_blank\" rel=\"noopener\">000webhost suffered a major data breach</a> that exposed over 13 million customer records. The data was sold and traded before 000webhost was alerted in October. The breach included names, email addresses and plain text passwords.",
+          "Domain" => "000webhost.com", "IsActive" => true, "IsRetired" => false,
+          "IsSensitive" => false, "IsSpamList" => false, "IsVerified" => true,
+          "LogoType" => "png", "Name" => "000webhost", "PwnCount" => 13545468,
+          "Title" => "000webhost"},
+        [retry_after: 0]
+      }
   """
   def breaches, do: do_get("breaches")
 
   @doc """
   Filters the result set to only breaches against the domain specified
+
+  ## Example
+
+      iex> ExPwned.Breaches.breaches("000webhost.com")
+      {:ok,
+       [%{"AddedDate" => "2015-10-26T23:35:45Z", "BreachDate" => "2015-03-01",
+          "DataClasses" => ["Email addresses", "IP addresses", "Names", "Passwords"],
+          "Description" => "In approximately March 2015, the free web hosting provider <a href=\"http://www.troyhunt.com/2015/10/breaches-traders-plain-text-passwords.html\" target=\"_blank\" rel=\"noopener\">000webhost suffered a major data breach</a> that exposed over 13 million customer records. The data was sold and traded before 000webhost was alerted in October. The breach included names, email addresses and plain text passwords.",
+          "Domain" => "000webhost.com", "IsActive" => true, "IsRetired" => false,
+          "IsSensitive" => false, "IsSpamList" => false, "IsVerified" => true,
+          "LogoType" => "png", "Name" => "000webhost", "PwnCount" => 13545468,
+          "Title" => "000webhost"}], [retry_after: 0]}
   """
   def breaches(domain), do: do_get("breaches", %{domain: domain})
 
   @doc """
   Sometimes just a single breach is required and this can be retrieved by the breach "name".
   This is the stable value which may or may not be the same as the breach "title" (which can change).
+
+  ## Example
+
+      iex> ExPwned.Breaches.breach("000webhost")
+      {:ok,
+       %{"AddedDate" => "2015-10-26T23:35:45Z", "BreachDate" => "2015-03-01",
+         "DataClasses" => ["Email addresses", "IP addresses", "Names", "Passwords"],
+         "Description" => "In approximately March 2015, the free web hosting provider <a href=\"http://www.troyhunt.com/2015/10/breaches-traders-plain-text-passwords.html\" target=\"_blank\" rel=\"noopener\">000webhost suffered a major data breach</a> that exposed over 13 million customer records. The data was sold and traded before 000webhost was alerted in October. The breach included names, email addresses and plain text passwords.",
+         "Domain" => "000webhost.com", "IsActive" => true, "IsRetired" => false,
+         "IsSensitive" => false, "IsSpamList" => false, "IsVerified" => true,
+         "LogoType" => "png", "Name" => "000webhost", "PwnCount" => 13545468,
+         "Title" => "000webhost"}, [retry_after: 0]}
   """
   def breach(name), do: do_get("breach/#{name}")
 
@@ -39,6 +77,23 @@ defmodule ExPwned.Breaches do
   For example, many breaches expose data classes such as "Email addresses" and "Passwords".
   The values returned by this service are ordered alphabetically in a string array
   and will expand over time as new breaches expose previously unseen classes of data.
+
+      iex> ExPwned.Breaches.dataclasses
+      {:ok,
+       ["Account balances", "Age groups", "Astrological signs", "Avatars",
+        "Bank account numbers", "Banking PINs", "Beauty ratings", "Biometric data",
+        "Browser user agent details", "Car ownership statuses", "Career levels",
+        "Chat logs", "Credit card CVV", "Credit cards", "Credit status information",
+        "Customer feedback", "Customer interactions", "Dates of birth",
+        "Deceased date", "Device information", "Device usage tracking data",
+        "Drinking habits", "Drug habits", "Education levels", "Email addresses",
+        "Email messages", "Employers", "Ethnicities", "Family members' names",
+        "Family plans", "Family structure", "Financial transactions",
+        "Fitness levels", "Genders", "Geographic locations", "Government issued IDs",
+        "Historical passwords", "Home ownership statuses", "Homepage URLs",
+        "Income levels", "Instant messenger identities", "IP addresses", "Job titles",
+        "MAC addresses", "Marital statuses", "Names", "Nicknames", "Parenting plans",
+        ...], [retry_after: 0]}
   """
   def dataclasses, do: do_get("dataclasses")
 end
--- mix.exs
+++ mix.exs
@@ -6,29 +6,18 @@ defmodule ExPwned.Mixfile do
      version: "0.1.0",
      elixir: "~> 1.3",
      description: "Elixir client for haveibeenpwned.com",
-     source_url: "https://github.com/techgaun/xmart-things",
+     source_url: "https://github.com/techgaun/ex_pwned",
      package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     docs: [extras: ["README.md"]],
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:httpoison, "~> 0.9 or ~> 0.10"},

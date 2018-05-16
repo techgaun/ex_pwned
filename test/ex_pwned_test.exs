e26035372edc35c0b5167446ee852f133800aa03
Grant Nelson <grantnelson@Grants-MacBook-Pro.local>
2018-05-16 18:13:15 -0400

    added top-level function tests

--- test/ex_pwned_test.exs
+++ test/ex_pwned_test.exs
@@ -1,5 +1,12 @@
 defmodule ExPwnedTest do
   use ExUnit.Case
 
-  
+  test "password_breached?/1" do
+    assert true == ExPwned.password_breached?("123456")
+  end
+
+  test "password_breach_count/1" do
+    result = ExPwned.password_breach_count("123456")
+    assert result > 0
+  end
 end

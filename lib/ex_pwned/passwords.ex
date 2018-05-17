b21513c517d7f9a7b25450a7cfd6c88ea53cf35c
Grant Nelson <grantmnelsn@gmail.com>
2018-05-17 15:56:17 -0400

    Adds Support For PwnedPasswords API (#3)
    * first draft of passwords module implementing the password range (with k-anonymity) HIBP endpoint
    
    * tests and docs written for passwords moduel
    
    * added more documentation
    
    * added readme documentation of newest top-level API methods
    
    * typo in documentation fix.
    
    * added top-level function tests

---
+++ lib/ex_pwned/passwords.ex
@@ -0,0 +1,64 @@
+defmodule ExPwned.Passwords do
+  @moduledoc """
+    Module to interact with HIBP API to retrive breached passwords data.
+  """
+
+  @doc """
+    Returns true|false, or {:error, "reason for error, probably HTTP/Network related"}
+  """
+  def breached?(password) do
+    case password_breach_count(password) do
+      0 -> false
+      x when x > 0 -> true
+      {:error, error} -> {:error, error}
+      unexpected -> unexpected
+    end
+  end
+
+  @doc """
+    Returns an integer representing the number of times this password has been
+    seen in a breach. Can be zero, representing no breaches.
+  """
+  def password_breach_count(password) do
+    {partial_hash, hash_suffix} =
+      password
+      |> hash_sha1()
+      |> String.split_at(5)
+
+    call_api(partial_hash, hash_suffix)
+  end
+
+  def call_api(partial_hash, hash_suffix) do
+    case HTTPoison.get("https://api.pwnedpasswords.com/range/" <> partial_hash) do
+      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
+        handle_success(body, hash_suffix)
+      {:ok, %HTTPoison.Response{body: body, status_code: 429}} ->
+        {:error, body}
+      other ->
+        {:error, other}
+    end
+  end
+
+  def handle_success(body, hash_suffix) do
+    [_suffix, count] =
+      body
+      |> parse_body()
+      |> Enum.find(["", "0"], &matches_suffix(&1, hash_suffix)) # default: ["", "0"]
+
+    count |> String.to_integer()
+  end
+
+  def parse_body(body) do
+    body
+    |> String.split(~r/\r\n/, trim: true)
+    |> Enum.map(&String.split(&1, ":"))
+  end
+
+  defp matches_suffix([line_suffix, _count], search_suffix) do
+    line_suffix == search_suffix
+  end
+
+  defp hash_sha1(word) do
+    :crypto.hash(:sha, word) |> Base.encode16
+  end
+end
--- test/ex_pwned_test.exs
+++ test/ex_pwned_test.exs
@@ -1,8 +1,12 @@
 defmodule ExPwnedTest do
   use ExUnit.Case
-  doctest ExPwned
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "password_breached?/1" do
+    assert true == ExPwned.password_breached?("123456")
+  end
+
+  test "password_breach_count/1" do
+    result = ExPwned.password_breach_count("123456")
+    assert result > 0
   end
 end
